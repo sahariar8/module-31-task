25f90db26f5a077b461d72d0dd31ba08c5ed67d9
Sahariar <sahariaralam8@gmail.com>
2023-08-24 14:18:33 +0600

    2nd

--- task.js
+++ task.js
@@ -72,3 +72,27 @@ console.log(sum);
 //Alhamdulah done
 
 
+//distructuring...
+
+const {name,position} = people;
+// console.log(name);
+function min(nums) { 
+    return Math.min(nums)
+ }
+ const cube=x=> x*x*x; 
+ console.log(cube(2))
+ const [a, b] = [1,2,3,4,45,5]; 
+ console.log(a+b);
+
+ const {x, y, z} = {x: 1, y1: 2, z: 3};
+ console.log(y)
+ const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
+
+const friends = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
+const newfr =friends.find(friend=>friend.length==5)
+console.log(newfr)
+const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
+const {price} = product;
+console.log(price)
